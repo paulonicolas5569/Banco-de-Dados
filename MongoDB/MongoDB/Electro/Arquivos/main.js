bf71d68db2aec2914cf519e6c169c88576f9973f
Paulo Nicolas <115593922+paulonicolas5569@users.noreply.github.com>
2024-02-27 19:18:08 -0300

    Update main.js

--- MongoDB/MongoDB/Electro/Arquivos/main.js
+++ MongoDB/MongoDB/Electro/Arquivos/main.js
@@ -1,110 +1,141 @@
-// importar o electron
-const {
-    app,
-    BrowserWindow,
-    Menu
-} = require('electron')
+/**
+ * Arquivo principal de configuração do electron
+ */
 
-//janela principal
-//uma função que ira executar e gerar nossa janela principal
+//processos (back-end)
+console.log("Hello Electron! (Processo principal node.js(back-end))")
+
+// importar o electron
+const { app, BrowserWindow, Menu, shell, ipcMain } = require('electron')
+// definir o path (preload.js)
+const path = require('node:path')
+
+
+// janela principal
 const mainWindow = () => {
-    //criar um objeto de nome win (configurações da janela)
+    // criar um objeto de nome win (configurações da janela)
     const win = new BrowserWindow({
         width: 800, //largura da janela
-        height: 1080, // altura da janela
-        resizable: false, //desabilita o redimensionamento da tela
-        title: "Tutorial Electron", //muda o titulo da janela
-        autoHideMenuBar: false, //esconde a barra de menu na parte superior
-        icon: `${__dirname}/src/public/img/fogo.png`, //inserir um ícone ${__dirname} => caminho absoluto
-
+        height: 600, //altura da janela
+        resizable: false, //evitar redimensionamento
+        title: "Tutorial Electron", //mudar titulo
+        //autoHideMenuBar: true, //esconder ou substituir menu
+        icon: `${__dirname}/src/public/img/pc.png`, //inserir um icone ${__dirname} -> caminho absoluto
+        webPreferences:{
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,//Permite executar recursos do node em app.js
+            contextIsolation: false
+        }
     })
-
-    //carregar documento html na janela principal
+    //carregar um documento html na janela principal
     win.loadFile(`${__dirname}/src/views/index.html`)
-    const menuPersonalizado = Menu.buildFromTemplate(menuTemplate) //Carregar o menu personalizados (Linha 46)
+
+    //carregar menu personalizado
+    const menuPersonalizado = Menu.buildFromTemplate(menuTemplate)
     Menu.setApplicationMenu(menuPersonalizado)
-}
+} //fim da janela principal
 
-let about //variavel para resolver o problema de abrir multiplas janelas
-
-
-//criar a janela sobre - conteudo duplicado da janela principal
+// janela sobre
+let about //variavel de apoio para resolver o problema de abrir múltiplas instancias da janela sobre
 const aboutWindow = () => {
-    //se não existir nenhuma janela criada, cria uma nova e se existir ele não cria (! inverte a logica)
+    //se não existir nenhuma janela criada - criar uma nova e se existir não crie (!inverter a lógica)
     if (!about) {
-        //criar um objeto de nome win (configurações da janela)
+        // criar um objeto de nome win (configurações da janela)
         about = new BrowserWindow({
             width: 360, //largura da janela
-            height: 320, // altura da janela
-            resizable: false, //desabilita o redimensionamento da tela
-            title: "Sobre", //muda o titulo da janela
-            autoHideMenuBar: true, //esconde a barra de menu na parte superior
-            icon: `${__dirname}/src/public/img/fogo.png` //inserir um ícone ${__dirname} => caminho absoluto
+            height: 320, //altura da janela
+            resizable: false, //evitar redimensionamento
+            title: "Sobre", //mudar titulo
+            autoHideMenuBar: true, //esconder ou substituir menu
+            icon: `${__dirname}/src/public/img/pc.png` //inserir um icone ${__dirname} -> caminho absoluto
         })
-
-        //carregar documento html na janela principal
+        //carregar um documento html na janela principal
         about.loadFile(`${__dirname}/src/views/sobre.html`)
-        //reseta a variavel about para resolver o problema de reabertura da tela Sobre
-        about.on('closed', ()=>{ //assim que fechar a aba ele vai executar a função 
+        //o codígo abaixo resolve o problema de reabertura da tela sobre
+        about.on('closed', () => {
             about = null
         })
-        }
-
-
+    }
 }
+
+
 //exibir a janela principal
-//quando o app estiver pronto ele devera executar a arrow function mainWindow()
 app.whenReady().then(() => {
     mainWindow()
+    // MAC OS - Ativar aplicativos se nenhuma janela estiver aberta 
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
 })
 
-//menu presonalizado
-//criando um vetor
+//Encerrar Aplicativo Ao fechar janlas (Windows w Linux)
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+    })
+
+//Menu personalizado
 const menuTemplate = [
-    //objeto do vetor
     {
-        //coloca o Arquivo na barra de menu
         label: 'Arquivo',
-        submenu: [ //menu que ira aparece quando criar o Arquivo
+        submenu: [
             {
-                label: 'sair',
-                click: () => app.quit(), //script para que a aba seja fechada quando clicada na label
-                accelerator: 'Alt+F4' //mostra a tecla de atalho para executar o comando
+                label: 'Sair',
+                click: () => app.quit(),
+                accelerator: 'Alt+F4'
             }
         ]
     },
     {
         label: 'Exibir',
-        submenu: [{
-            label: 'Recarregar', //adiciona um submenu para recarregar e quando clicado ele recarrega
-            role: 'reload' //atribui a acclerator automaticamente como uma regra, e ja atribuindo sua função
+        submenu: [
+            {
+                label: 'Recarregar',
+                role: 'reload',
+                accelerator: 'F5'
             },
-        {
-            label: 'Ferramentas do desenvolvedor',
-            role: "toggleDevTools" 
-        
-        },
-        {
-            type: 'separator' //desenha uma linha
-        },
-        {
-            label: 'Aplicar zoom',
-            role: 'zoomIn'
-        },
-        {
-            label: 'Retirar zoom',
-            role: 'zoomOut'
-        },
-        {
-            label: 'Restaurar zoom',
-            role: 'resetZoom'
-        }]
+            {
+                label: 'Ferramentas do desenvolvedor',
+                role: 'toggleDevTools',
+                accelerator: 'Ctrl+Shift+i'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Aplicar zoom',
+                role: 'zoomIn'
+            },
+            {
+                label: 'Reduzir',
+                role: 'zoomOut'
+            },
+            {
+                label: 'Restaurar o zoom padrão',
+                role: 'resetZoom'
+            }
+        ]
     },
     {
         label: 'Ajuda',
-        submenu: [{
-            label: 'Sobre',
-            click: aboutWindow //assim que clicado, chama a janela aboutWindow
-        }]
+        submenu: [
+            {
+                label: 'Documentos',
+                click: () => shell.openExternal("https://www.electronjs.org/pt/docs/latest/")
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Sobre',
+                click: aboutWindow
+            }
+        ]
     }
 ]
+
+//Exemplo de comunicação entre processos (front-ent e back-end)
+ipcMain.on('send-message', (event, message) => {
+    console.log("Processo Principal recebeu a mensagem:", message)
+    // eEncviar uma resposta para o processo de renderização 
+    event.sender.send('receive-message',"Olá Renderizador!!")
+})
